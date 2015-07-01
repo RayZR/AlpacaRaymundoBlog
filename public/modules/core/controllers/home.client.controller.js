03233d8b567127de2afae20a98cb143d9c648ad1
Ray Zhang <ray@Fran-iMac-2.local>
2015-07-01 15:01:36 +1000

    add scroll up& scroll down event

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$state',
+	function($scope, Authentication, $state) {
 
         $scope.pages = ['index', 'about', 'skill', 'experience', 'contact'];
 
@@ -35,31 +35,58 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
             }
         );
 
+
+        $scope.$on('scrollDown', function(){
+
+            var currentNum  = $scope.pages.indexOf($scope.currentState.name.split('.')[1]);
+            var nextNum     = currentNum + 1 ;
+
+            if( nextNum <= $scope.pages.length-1 ){
+                var nextState   = 'home.' + $scope.pages[nextNum];
+                $state.go(nextState);
+            }
+
+            console.log('scrollDown');
+        });
+
+        $scope.$on('scrollUp', function(){
+
+            var currentNum  = $scope.pages.indexOf($scope.currentState.name.split('.')[1]);
+            var nextNum     = currentNum - 1 ;
+
+            if( nextNum >= 0){
+                var nextState   = 'home.' + $scope.pages[nextNum];
+                $state.go(nextState);
+            }
+
+            console.log('scrollUp');
+
+        });
+
         var scrollTimer = '';
 
-        $(window).on( 'mousewheel', function ( event ) {
+        $(window).on( 'DOMMouseScroll mousewheel onmousewheel', function ( event ) {
 
             if (scrollTimer) { clearTimeout(scrollTimer); }
 
                if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) {
-                      //scroll down
 
+                   //scroll down
                    scrollTimer = setTimeout(function() {
-                       $(window).trigger('scrollUp');
+                       $scope.$emit('scrollDown');
                    }, 200);
 
-                      } else {
-                         //scroll up
+               } else {
+
+                   //scroll up
                    scrollTimer = setTimeout(function() {
-                       $(window).trigger('scrollDown');
+                       $scope.$emit('scrollUp');
                    }, 200);
 
-                      }
+               }
                                         //prevent page fom scrolling
-                      return false;
-               });
-
-
+               return false;
+        });
 
 	}
 ]);
